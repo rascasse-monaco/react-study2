7009b0fb6c6628e518784f21f43d635c8d70fbc3
rascasse-monaco <mktwkhr@gmail.com>
2021-01-16 03:03:51 +0900

    ５まで終了

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillMount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render() {
+  return (
+    <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+  </div>
+  );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
